74258f6226919a11d06d6831be11e8ef56335c88
tugbayilmaz01 <tugbayilmaz4@posta.mu.edu.tr>
2024-07-20 21:55:31 +0300

    feat: added home page component with initial content

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+const HomePage: FC = () => {
+	return (
+		<div>
+			<h1>Welcome to BugPilot</h1>
+			<p>This is your bug tracking dashboard.</p>
+		</div>
+	);
+};
+
+export default HomePage;
